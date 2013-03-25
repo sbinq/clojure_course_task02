18f62bf242c74f14a90568e597ed84c39f5b215d
Stas Binko <stas.binko@gmail.com>
2013-03-26 01:27:23 +0300

    version with futures instead of thread pool - but still with java-style wait/notify

--- src/clojure_course_task02/core.clj
+++ src/clojure_course_task02/core.clj
@@ -4,7 +4,6 @@
   (:gen-class))
 
 (set! *warn-on-reflection* true)
-(def ^:dynamic *thread-pool-size* 6)
 
 (defn dirs-and-files [^File parent]
   "List of two vectors - directories and files in given directory
@@ -21,14 +20,9 @@
       (not (nil? (re-matches pattern (.getName file)))))))
 
 (defn find-files-mt [file-filter root]
-  "Uses multiple workers in different threads synchronizing state with refs.
-   Main thread uses java-style wait/notify to check if everything is finished
-   which seems to be ok - https://groups.google.com/forum/?fromgroups=#!topic/clojure/foqaJ4DQoWM "
-  ;; do not want to use pmap here since this would lead to many threads created at the same time;
-  ;; plain clojure future does not allow to restrict threads count - so using java executors explicitly
-  (let [pool (Executors/newFixedThreadPool *thread-pool-size*)
-        matching-files-ref (ref [])
-        submitted-workers-count-ref (ref 1) ; for initial worker handling root dir
+  "Uses multiple workers in different threads synchronizing state with refs"
+  (let [matching-files-ref (ref [])
+        submitted-workers-count-ref (ref 1) ; this is for root worker
         worker-finished-signal (Object.)
         worker (fn worker [^File parent]
                  (let [[dirs files] (dirs-and-files parent)
@@ -36,29 +30,32 @@
                    (dosync
                     ;; don't actually need refs here - two independents atoms would do fine
                     ;; (latest two invocations would be united into one)
-                    (alter matching-files-ref conj matching-files) ;TODO: if use concat here - getting StackOverflow on large input
-                    (alter submitted-workers-count-ref + (count dirs))
-                    (alter submitted-workers-count-ref dec)) ; for current worker
-                   (doseq [dir dirs]
-                     (.execute pool #(worker dir)))
+                    (alter matching-files-ref conj matching-files) ; TODO: if use concat here - getting StackOverflow on large input;
+                    (alter submitted-workers-count-ref + (count dirs)) ; new workers to be added, increasing counter
+                    (alter submitted-workers-count-ref dec)) ; but this one is finishing, decreasing counter
+                   (doall (map (fn [dir] (future (worker dir))) dirs))
                    (locking worker-finished-signal
                      (.notify worker-finished-signal))))]
     (locking worker-finished-signal
-      (.execute pool #(worker root))
+      (future (worker root))
       (loop [submitted-workers-count @submitted-workers-count-ref]
         (if (> submitted-workers-count 0)
           (do
             (.wait worker-finished-signal)
             (recur @submitted-workers-count-ref))
-          (do
-            (.shutdown pool)
-            (flatten @matching-files-ref))))))) ;TODO: flatten here because of concat workaround above
+          (flatten @matching-files-ref)))))) ; TODO: flatten here because of concat workaround above
+
+(defn find-files-pmap2 [file-filter ^File root]
+  (apply concat
+         (pmap #(let [[dirs files] (dirs-and-files %)]
+                  (filter file-filter files))
+               (filter (fn [^File file] (.isDirectory file)) (file-seq root))))) ; silly to filter out files this way - but just for testing
 
 (defn find-files [file-name ^String path]
   "Implements searching for a file using his name as a regexp."
   (let [file-filter (make-regex-file-filter file-name)
         root (File. path)
-        files (find-files-mt file-filter root)]
+        files (find-files-pmap2 file-filter root)]
     (map (fn [^File file] (.getName file)) files)))
 
 (defn usage []
@@ -70,4 +67,6 @@
     (usage)
     (do
       (println "Searching for " file-name " in " path "...")
-      (dorun (map println (find-files file-name path))))))
+      (dorun (map println (find-files file-name path)))
+      (println "Finished with" (count (Thread/getAllStackTraces)) "threads")
+      (shutdown-agents))))
